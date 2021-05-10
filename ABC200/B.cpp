4513361cc96fc7e3469f1e57283ecfad9752cfe5
ytsmash <77914653+ytsmash@users.noreply.github.com>
2021-05-11 08:49:37 +0900

    Create B.cpp

---
+++ ABC200/B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long N;
+    int K;
+    cin >> N >> K;
+    
+    for (int i = 0; i < K; ++i) {
+        if (N % 200 == 0) {
+            N /= 200;
+        }
+        else {
+            N = N * 1000 + 200;
+        }
+    }
+    
+    cout << N;
+}
